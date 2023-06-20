9a8d21a1d9b0520a861efc50462acb01218c3a0e
mysh <nshyahn1@gmail.com>
2023-06-20 11:30:02 +0300

    Fix env variables

--- lambda/booksapi/js/api.js
+++ lambda/booksapi/js/api.js
@@ -10,7 +10,7 @@ process.env.DB_NAME = 'books'
 const db_host     = process.env.DB_HOST;
 const db_port     = process.env.DB_PORT;
 const db_user     = process.env.DB_USER;
-const db_password = process.env.DB_PASSWORD;
+const db_password = process.env.DB_PASS;
 const db_name     = process.env.DB_NAME;
 
 
--- lambda/booksapi/py/booksapi.py
+++ lambda/booksapi/py/booksapi.py
@@ -7,7 +7,7 @@ import datetime
 db_host     = os.environ['DB_HOST']
 db_port     = os.environ['DB_PORT']
 db_user     = os.environ['DB_USER']
-db_password = os.environ['DB_PASSWORD']
+db_password = os.environ['DB_PASS']
 db_name     = os.environ['DB_NAME']
 
 db_connection = pymysql.connect(
